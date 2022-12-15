e262ccc462e874a96d1a5dead9a40b6c9686d0ef
Rene Huber <huberskull@gmail.com>
2022-12-15 02:57:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
+
+export const ThemeContext = createContext(null);
+
+function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((state) => (state === "light" ? "dark" : "light"));
+  };
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <div className="switch">
+          <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        </div>
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
